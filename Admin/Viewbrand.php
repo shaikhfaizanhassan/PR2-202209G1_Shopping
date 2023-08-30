b14b4326b28a04a274516c47c0ebfa338f87c7e7
faiz ali <shaikhfaiz793@gmail.com>
2023-08-30 19:39:24 +0500

    heone

--- Admin/Viewbrand.php
+++ Admin/Viewbrand.php
@@ -1,52 +1,58 @@
-<?php 
+<?php
 include("connection.php");
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <div class="container">
         <div class="col-md-12">
-        <div class="ibox">
-        <div class="ibox-head">
+            <div class="ibox">
+                <div class="ibox-head">
 
-                                <div class="ibox-title">View Brand</div>
-                            </div>
-                            <div class="ibox-body">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>Name</th>
-                                            <th>Action</th>
-                                        </tr>
-                                     </thead>
-                                    <tbody>
-                                    <?php 
-                                            $fetchcat = mysqli_query($con,"select * from brand");
-                                            while($row = mysqli_fetch_array($fetchcat))
-                                            {
-                                        ?>
-                                        <tr>
-                                            <td><?php echo $row[0] ?></td>
-                                            <td><?php echo $row[1] ?></td>
-                                            <td>
-                                                <a href="" class="btn btn-success">Edit</a>
-                                                <a href="" class="btn btn-danger">Delete</a>
-                                            </td>
-                                        </tr>
+                    <div class="ibox-title">View Brand</div>
+                </div>
+                <div class="ibox-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $fetchcat = mysqli_query($con, "select * from brand");
+                            while ($row = mysqli_fetch_array($fetchcat)) {
+                                ?>
+                                <tr>
+                                    <td>
+                                        <?php echo $row[0] ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $row[1] ?>
+                                    </td>
+                                    <td>
+                                        <a href="" class="btn btn-success">Edit</a>
+                                        <a href="" class="btn btn-danger">Delete</a>
+                                    </td>
+                                </tr>
 
-                                        <?php } ?>
-                                    </tbody>
-                                </table>
-                            </div>    
-    </div>    
-    </div>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
+
 </html>
--- Admin/Viewproduct.php
+++ Admin/Viewproduct.php
@@ -1,50 +1,79 @@
+<?php 
+    include("connection.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <div class="container">
         <div class="col-md-12">
-        <div class="ibox">
-        <div class="ibox-head">
+            <div class="ibox">
+                <div class="ibox-head">
 
-                                <div class="ibox-title">View Product</div>
-                            </div>
-                            <div class="ibox-body">
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>Name</th>
-                                            <th>Price</th>
-                                            <th>Description</th>
-                                            <th>Cat ID</th>
-                                            <th>Brand ID</th>
-                                            <th>Image</th>
-                                        </tr>
-                                     </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Nokia 3310</td>
-                                            <td>1100</td>
-                                            <td>pathar</td>
-                                            <td>2</td>
-                                            <td>3</td>
-                                            <td><img src="image/3310.jpg " width="100"  alt="" ></td>
-                                            <td>
-                                                <a href="" class="btn btn-success">Edit</a>
-                                                <a href="" class="btn btn-danger">Delete</a>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>    
-    </div>    
-    </div>
+                    <div class="ibox-title">View Product</div>
+                </div>
+                <div class="ibox-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th>Description</th>
+                                <th>Category</th>
+                                <th>Brand</th>
+                                <th>Image</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php 
+                                $fetchpro = mysqli_query($con,"SELECT pid,pname,pprice,pdesc,catname,brandname,image FROM product
+                                INNER JOIN
+                                category
+                                ON
+                                product.Category_ID = category.cid
+                                INNER JOIN
+                                brand
+                                on 
+                                product.brand_id = brand.bid");
+
+                                while($row = mysqli_fetch_array($fetchpro))
+                                {
+
+                                
+                            ?>
+                            <tr>
+                                <td><?php echo $row[0] ?></td>
+                                <td><?php echo $row[1] ?></td>
+                                <td><?php echo $row[2] ?></td>
+                                <td><?php echo $row[3] ?></td>
+                                <td><?php echo $row[4] ?></td>
+                                <td><?php echo $row[5] ?></td>
+                                <td>
+                                    <img src="proimage/<?php echo $row[6] ?>" width="80" alt="">
+                                </td>
+                                <td>
+                                    <a href="" class="btn btn-success">Edit</a>
+                                    <a href="" class="btn btn-danger">Delete</a>
+                                    
+                                </td>
+                                
+                               
+                            </tr>
+                            <?php  } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
+
 </html>
